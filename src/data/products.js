e71f221a3c5927d91acac1bf2e776973b9bbfc5d
MoizesLopes <moizes340@gmail.com>
2025-08-28 12:57:47 -0300

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -8,5 +8,5 @@ export const PRODUCTS = [
   { id:'p5', name:'Salgadinho Sticks Pizza', description:'50g.', price:2.50, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
   { id:'p6', name:'Salgadinho Pururuca', description:'150g.', price:8.00, category:'Bebidas', img:'https://placehold.co/600x400/png?text=Cafe' },
   { id:'p5', name:'Trento Alegro', description:'26g.', price:3.50, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
-  { id:'p5', name:'Trento T-Pro', description:'26g.', price:4.50, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
+  { id:'p5', name:'Trento T-Pro', description:'26g.', price:4.50, category:'Acessórios', img:'https://images.tcdn.com.br/img/img_prod/1106817/chocolate_trento_allegro_choco_e_amendoim_35g_8763_1_8cecf7eeaaf941efbef9a05ff87cdc38.jpg' },
 ]
