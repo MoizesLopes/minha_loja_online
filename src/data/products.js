1db8929632480784ec6995b7ab4cc945a004f857
MoizesLopes <moizes340@gmail.com>
2025-08-28 12:55:22 -0300

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,8 +1,12 @@
 export const PRODUCTS = [
-  { id:'p1', name:'Paçoca Big Rolha', description:'52g', price:18.9, category:'Doces', img:'https://placehold.co/600x400/png?text=Paçoca Big Rolha+70%25' },
-  { id:'p2', name:'Paçoca Quadrada', description:'80g.', price:14.5, category:'Doces', img:'https://placehold.co/600x400/png?text=Paçoca Quadrada' },
-  { id:'p3', name:'Paçoca Pequena', description:'15g.', price:7.9, category:'Doces', img:'https://placehold.co/600x400/png?text=Paçoca Pequena' },
-  { id:'p4', name:'Camiseta Básica', description:'100% algodão – várias cores.', price:39.9, category:'Roupas', img:'https://placehold.co/600x400/png?text=Camiseta' },
-  { id:'p5', name:'Boné Trucker', description:'Ajustável – logo bordado.', price:59.9, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
-  { id:'p6', name:'Café Moído 250g', description:'Torra média – blend da casa.', price:24.9, category:'Bebidas', img:'https://placehold.co/600x400/png?text=Cafe' },
+  { id:'p1', name:'Paçoca Big Rolha', description:'52g', price:2.50, category:'Doces', img:'https://placehold.co/600x400/png?text=Paçoca Big Rolha+70%25' },
+  { id:'p2', name:'Paçoca Quadrada', description:'80g.', price:2.50, category:'Doces', img:'https://placehold.co/600x400/png?text=Paçoca Quadrada' },
+  { id:'p3', name:'Paçoca Pequena', description:'15g.', price:1.00, category:'Doces', img:'https://placehold.co/600x400/png?text=Paçoca Pequena' },
+  { id:'p4', name:'Salgadinho Presunto', description:'35g.', price:2.50, category:'Roupas', img:'https://placehold.co/600x400/png?text=Camiseta' },
+  { id:'p5', name:'Salgadinho Bacon', description:'35g.', price:2.50, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
+  { id:'p5', name:'Salgadinho Sticks Churrasco', description:'50g.', price:2.50, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
+  { id:'p5', name:'Salgadinho Sticks Pizza', description:'50g.', price:2.50, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
+  { id:'p6', name:'Salgadinho Pururuca', description:'150g.', price:8.00, category:'Bebidas', img:'https://placehold.co/600x400/png?text=Cafe' },
+  { id:'p5', name:'Trento Alegro', description:'26g.', price:3.50, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
+  { id:'p5', name:'Trento T-Pro', description:'26g.', price:4.50, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
 ]
