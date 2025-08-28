96a262cfd3fe986ab50f9ff478f6ba4a3aaa2628
MoizesLopes <moizes340@gmail.com>
2025-08-28 11:45:32 -0300

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,7 +1,7 @@
 export const PRODUCTS = [
-  { id:'p1', name:'Paçoca Big Rolha', description:'52g', price:18.9, category:'Doces', img:'https://placehold.co/600x400/png?text=Chocolate+70%25' },
-  { id:'p2', name:'Paçoca Premium', description:'Pacote com 10 unidades.', price:14.5, category:'Doces', img:'https://placehold.co/600x400/png?text=Pacoca' },
-  { id:'p3', name:'Salgadinho de Milho', description:'Sabor churrasco – 90g.', price:7.9, category:'Salgadinhos', img:'https://placehold.co/600x400/png?text=Salgadinho' },
+  { id:'p1', name:'Paçoca Big Rolha', description:'52g', price:18.9, category:'Doces', img:'https://placehold.co/600x400/png?text=Paçoca Big Rolha+70%25' },
+  { id:'p2', name:'Paçoca Quadrada', description:'80g.', price:14.5, category:'Doces', img:'https://placehold.co/600x400/png?text=Paçoca Quadrada' },
+  { id:'p3', name:'Paçoca Pequena', description:'15g.', price:7.9, category:'Doces', img:'https://placehold.co/600x400/png?text=Paçoca Pequena' },
   { id:'p4', name:'Camiseta Básica', description:'100% algodão – várias cores.', price:39.9, category:'Roupas', img:'https://placehold.co/600x400/png?text=Camiseta' },
   { id:'p5', name:'Boné Trucker', description:'Ajustável – logo bordado.', price:59.9, category:'Acessórios', img:'https://placehold.co/600x400/png?text=Bone' },
   { id:'p6', name:'Café Moído 250g', description:'Torra média – blend da casa.', price:24.9, category:'Bebidas', img:'https://placehold.co/600x400/png?text=Cafe' },
